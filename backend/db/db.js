88b93c831cf85c29111c3e1c0c67a59b3fd73f8d
Khushi Kumari <120251449+Kumarikhushi712@users.noreply.github.com>
2025-04-24 12:51:00 +0530

    db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect('add connection string here')
+        await mongoose.connect('mongodb+srv://khushi087singh:KU8yRYdtoK4SyEJ8@cluster0.wupg8we.mongodb.net/')
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
